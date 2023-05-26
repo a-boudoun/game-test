2c28de2e0d675d794ac8fedd5b31343339f5555f
Abderrahim Boudounit <aboudoun@e1r7p15.1337.ma>
2023-05-26 15:36:07 +0100

    configuring tsc config file

--- game/src/test.ts
+++ game/src/test.ts
@@ -1,47 +1,47 @@
-import * as Matter from 'matter-js';
+// import * as Matter from 'matter-js';
 
-var Engine = Matter.Engine,
-Render = Matter.Render,
-Runner = Matter.Runner,
-Bodies = Matter.Bodies,
-Composite = Matter.Composite;
-// Get the canvas element
+// var Engine = Matter.Engine,
+// Render = Matter.Render,
+// Runner = Matter.Runner,
+// Bodies = Matter.Bodies,
+// Composite = Matter.Composite;
+// // Get the canvas element
 
-// create an engine
-var engine = Engine.create();
+// // create an engine
+// var engine = Engine.create();
 
-// create a renderer
-var render = Render.create({
-element: document.body,
-engine: engine,
-});
+// // create a renderer
+// var render = Render.create({
+// element: document.body,
+// engine: engine,
+// });
 
-// create two boxes and a ground
-// Create bodies
-const boxA = Matter.Bodies.rectangle(40, 20, 80, 80);
-const boxB = Matter.Bodies.rectangle(45, 50, 80, 80);
-const ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+//TODO: fix this
+// // create two boxes and a ground
+// // Create bodies
+// const boxA = Matter.Bodies.rectangle(40, 20, 80, 80);
+// const boxB = Matter.Bodies.rectangle(45, 50, 80, 80);
+// const ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-// Add bodies to the world
-Matter.World.add(engine.world, [boxA, boxB, ground]);
+// // Add bodies to the world
+// Matter.World.add(engine.world, [boxA, boxB, ground]);
 
-// Start the engine
-Matter.Engine.run(engine);
+// // Start the engine
+// Matter.Engine.run(engine);
 
-// Start the renderer
-Matter.Render.run(render);
+// // Start the renderer
+// Matter.Render.run(render);
 
 
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+// // add all of the bodies to the world
+// Composite.add(engine.world, [boxA, boxB, ground]);
 
-// run the renderer
-Render.run(render);
+// // run the renderer
+// Render.run(render);
 
-// create runner
-var runner = Runner.create();
+// // create runner
+// var runner = Runner.create();
 
-// run the engine
-Runner.run(runner, engine);
-// Render on changes
-  
+// // run the engine
+// Runner.run(runner, engine);
+// // Render on changes
