93bd737eb1fc3951cc08d1edfa1de47ca0a24637
Abderrahim Boudounit <aboudoun@e2r5p15.1337.ma>
2023-05-27 23:33:40 +0100

    working with canvas

--- game/dist/test.js
+++ game/dist/test.js
@@ -1,30 +1,37 @@
+console.log(Matter);
 var Engine = Matter.Engine,
 Render = Matter.Render,
 Runner = Matter.Runner,
 Bodies = Matter.Bodies,
 Composite = Matter.Composite;
 
+
 // create an engine
-var engine = Engine.create(
-);
-engine.gravity.y = 1;
-engine.gravity.x = 0;
-engine.gravity.scale = 0.001;
+var canvaa = document.getElementById("Lcanvas");
+var engine = Engine.create();
 console.log(engine);
 // create a renderer
 var render = Render.create({
-element: document.body,
-engine: engine
+	engine: engine,
+	canvas: canvaa,
+	options: {
+		// makes the pixels look a bit nicer by rendering them as
+		pixelRatio: 2,
+		// wireframeBackground: '#10c7c7',
+		// background: 'transparent',
+	}
 });
-
 // create two boxes and a ground
 
-var boxF = Bodies.circle(450, 10, 15, 80);
+var boxA =  Bodies.circle(400, 20, 20, { inertia: Infinity });
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 var sqf = Bodies.rectangle(400, 0, 810, 60, { isStatic: true });
-boxF.restitution = 1.5;
+var wall1 = Bodies.rectangle(0, 300, 60, 810, { isStatic: true });
+boxA.restitution = 1;
+boxA.friction = 0;
+boxA.frictionAir = 0;
 // add all of the bodies to the world
-Composite.add(engine.world, [boxF, ground, sqf]);
+Composite.add(engine.world, [boxA, ground, sqf]);
 
 // run the renderer
 Render.run(render);
@@ -34,3 +41,4 @@ var runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
+console.log("done");
